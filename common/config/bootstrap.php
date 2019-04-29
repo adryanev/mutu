28f6fd5132b601a68a3b2a440243bb653a85f6b7
Adryan Eka Vandra <adryanekavandra@gmail.com>
2019-04-30 01:30:44 +0700

    Initial commit

---
+++ common/config/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@akreditasi', dirname(dirname(__DIR__)) . '/akreditasi');
+Yii::setAlias('@monitoring', dirname(dirname(__DIR__)) . '/monitoring');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
---
+++ monitoring/config/main.php
@@ -0,0 +1,50 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-monitoring',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'monitoring\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-monitoring',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-monitoring', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the monitoring
+            'name' => 'advanced-monitoring',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
