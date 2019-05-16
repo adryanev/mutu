f3c0383f2dd6946e30744522a07f544d55c70bf0
Adryan Eka Vandra <adryanekavandra@gmail.com>
2019-05-16 19:27:14 +0700

    set halaman standar

--- akreditasi/controllers/BorangS1ProdiController.php
+++ akreditasi/controllers/BorangS1ProdiController.php
@@ -3,6 +3,7 @@
 namespace akreditasi\controllers;
 
 use akreditasi\models\BorangS1ProdiForm;
+use common\models\BorangS1Prodi;
 use common\models\BorangS1ProdiStandar1;
 use common\models\BorangS1ProdiStandar2;
 use common\models\BorangS1ProdiStandar3;
@@ -19,9 +20,10 @@ class BorangS1ProdiController extends \yii\web\Controller
     public function actionIndex($borang)
     {
         $file_json = 'borang_prodi_s1.json';
+        $borangProdi = BorangS1Prodi::findOne($borang);
         $dokumenBorang = new BorangS1ProdiForm();
         $dataDokumenBorang = DokumenBorangS1Prodi::find()->where(['id_borang_s1_prodi'=>$borang])->all();
-        $json = file_get_contents(Yii::getAlias('@common/required/'.$file_json));
+        $json = file_get_contents(Yii::getAlias('@common/required/borang/'.$file_json));
         $standar1 = BorangS1ProdiStandar1::find()->where(['id_borang_s1_prodi'=>$borang])->one();
         $standar2 = BorangS1ProdiStandar2::find()->where(['id_borang_s1_prodi'=>$borang])->one();
         $standar3 = BorangS1ProdiStandar3::find()->where(['id_borang_s1_prodi'=>$borang])->one();
@@ -39,6 +41,7 @@ class BorangS1ProdiController extends \yii\web\Controller
             return $this->redirect(Url::current());
         }
         return $this->render('index',[
+            'borangProdi'=>$borangProdi,
             'dokumenBorang'=>$dokumenBorang,
             'dataDokumenBorang'=>$dataDokumenBorang,
             'standar1'=>$standar1,
@@ -50,6 +53,55 @@ class BorangS1ProdiController extends \yii\web\Controller
             'standar7'=>$standar7,
             'json'=>$json
         ]);
+    }
+
+    public function actionStandar1($borang){
+
+        $file_json = 'borang_prodi_s1.json';
+        $json = file_get_contents(Yii::getAlias('@common/required/borang/'.$file_json));
+
+        return $this->render('standar1');
+    }
+    public function actionStandar2($borang){
+
+        $file_json = 'borang_prodi_s1.json';
+        $json = file_get_contents(Yii::getAlias('@common/required/borang/'.$file_json));
+        return $this->render('standar2');
+    }
+    public function actionStandar3($borang){
+
+        $file_json = 'borang_prodi_s1.json';
+        $json = file_get_contents(Yii::getAlias('@common/required/borang/'.$file_json));
+        return $this->render('standar3');
+    }
+    public function actionStandar4($borang){
+
+        $file_json = 'borang_prodi_s1.json';
+        $json = file_get_contents(Yii::getAlias('@common/required/borang/'.$file_json));
+        return $this->render('standar4');
+
+    }
+    public function actionStandar5($borang){
+
+        $file_json = 'borang_prodi_s1.json';
+        $json = file_get_contents(Yii::getAlias('@common/required/borang/'.$file_json));
+        return $this->render('standar5');
+
+    }
+    public function actionStandar6($borang){
+
+        $file_json = 'borang_prodi_s1.json';
+        $json = file_get_contents(Yii::getAlias('@common/required/borang/'.$file_json));
+        return $this->render('standar6');
+
+    }
+    public function actionStandar7($borang){
+
+        $file_json = 'borang_prodi_s1.json';
+        $json = file_get_contents(Yii::getAlias('@common/required/borang/'.$file_json));
+        return $this->render('standar7');
+
+
     }
 
 }
