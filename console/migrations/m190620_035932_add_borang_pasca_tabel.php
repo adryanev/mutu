62ff3bacb3ae561bf7d42246b09267b9b2b7b8fa
Novrii <38958308+Novrii@users.noreply.github.com>
2019-06-20 11:00:20 +0700

    tambah file migrasi pasca

---
+++ console/migrations/m190620_035932_add_borang_pasca_tabel.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190620_035932_add_borang_pasca_tabel
+ */
+class m190620_035932_add_borang_pasca_tabel extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190620_035932_add_borang_pasca_tabel cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190620_035932_add_borang_pasca_tabel cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
