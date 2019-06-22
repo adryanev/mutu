fbd469fa5f8907af42f3ed94ba6267a6d0dab0b5
Adryan Eka Vandra <adryanekavandra@gmail.com>
2019-06-22 07:56:56 +0700

    begin implementing rbac

--- common/config/main.php
+++ common/config/main.php
@@ -9,9 +9,9 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-//        'authManager'=>[
-//            'class'=>\yii\rbac\DbManager::class,
-//        ]
+        'authManager'=>[
+            'class'=>\yii\rbac\DbManager::class,
+        ]
     ],
     'modules'=>[
         'admin'=>[
